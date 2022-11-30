01becd773ac7feac9037e011232967ac663d0360
Durney <m.durney@ulster.ac.uk>
2022-11-30 17:13:17 +0000

    updates

--- js/app.js
+++ js/app.js
@@ -88,8 +88,8 @@ $(document).ready(function () {
 // AOS animation init
 $(document).ready(function () {
   AOS.init({
-    offset: 200, // offset (in px) from the original trigger point
-    duration: 600, // values from 0 to 3000, with step 50ms
+    offset: 100, // offset (in px) from the original trigger point
+    duration: 400, // values from 0 to 3000, with step 50ms
     easing: 'linear', // default easing for AOS animations
     once: true,
     delay:0,
